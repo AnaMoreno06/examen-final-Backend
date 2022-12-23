98b64c03e62e329770dac4545941a7fd0f7f8498
AnaMoreno06 <anamariamoca@ufps.edu.co>
2022-12-23 10:33:04 -0500

    prueba1

---
+++ examenfinalbackend/src/main/java/com/example/examenfinalbackend/ExamenfinalbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenfinalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenfinalbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenfinalbackendApplication.class, args);
+	}
+
+}
---
+++ examenfinalbackend/src/test/java/com/example/examenfinalbackend/ExamenfinalbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenfinalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenfinalbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
